5088e1356c3ea45d44b37e82a07e3e22e783a09b
Esrom de Paiva <esromqkd@gmail.com>
2021-06-15 14:48:46 -0300

    Initial project setup

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Entitites/EmployeeRole.cs
@@ -0,0 +1,16 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Entitites
+{
+    public record EmployeeRole
+    {
+
+        public string Id { get; private set; }
+
+        public string RoleName { get; init; }
+
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+    }
+}
