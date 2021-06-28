8aedfc31580699438178e6a5fd23a92807d267a3
Esrom de Paiva <esromqkd@gmail.com>
2021-06-28 13:10:56 -0300

    Aula 6

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,18 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole: BaseEntity<string>
+    {
+        public string RoleName { get; init; }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Services
+{
+    public class EmployeeRoleService : IEmployeeRoleService
+    {
+        public Task<EmployeeRole.Entities.EmployeeRole> AddEmployeeRole(EmployeeRole.Entities.EmployeeRole employeeRole)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
